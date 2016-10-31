358c0d738ac874865dfae16e6f844d9c3dcbe277
sanubhav96 <sanubhav96@gmail.com>
2016-10-31 19:31:33 +0530

    now correct :p

--- util/socket-util.js
+++ util/socket-util.js
@@ -1,5 +1,5 @@
 /**
- * Created by vipul on 6/08/2016.
+ * Created by vipul on 6/08/2016 with help sanubhav Khokhar.
  */
  module.exports = function(io) {
  	io.on('connection', function (socket) {
