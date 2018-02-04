3c055a252b0125b3a23671f8a6c2dd627fd29859
Vipul Maan <vipulmaan@outlook.com>
2018-02-04 13:29:12 +0530

    app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var express = require('express')
     ,   credentials = require('./credentials.js')
     ,   mongoose = require('mongoose');
 
-//Setup db________________
+//Setup db_______
 var options = {
     server: {
         socketOptions: { keepAlive: 1 }
